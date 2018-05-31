83bec5b5f935f6e39b1414a248ce50b273650b15
PenguinRun <shanban820@gmail.com>
2018-05-31 20:59:08 +0800

    increase the practice.

--- routes/firebase.js
+++ routes/firebase.js
@@ -7,10 +7,17 @@ var router = express.Router();
 
 const firebase = require('firebase')
 
+// set middleware
+router.use('/',function (req, res, next) {
+    const onTime = new Date()
+    console.log(onTime)
+    next()
+  })
+
 router.get('/', function (req, res, next) {
 
     var config = {
-        databaseURL: "https://cool-5c141.firebaseio.com"
+        databaseURL: "" // enter your databaseURL（輸入由firebase中申請到的firebase的databaseURL）
     };
 
     firebase.initializeApp(config);
@@ -18,17 +25,19 @@ router.get('/', function (req, res, next) {
     // Get a reference to the database service
     var database = firebase.database();
 
+     // set -> 建立新的資料
     firebase.database().ref('users/' + '123').set({
         username: 'wwwww',
         email: 'test@gmail.com'
     });
 
+    // update -> 更新指定資料
     firebase.database().ref('users/' + '223').update({
         username: 'wwwww',
         email: 'tes2t@gmail.com'
     });
 
-    // get data
+    // once -> 取得資料
     firebase.database().ref('users/').once('value', function (snapshot) {
         console.log(snapshot.val());
         res.json({
@@ -36,7 +45,8 @@ router.get('/', function (req, res, next) {
         })
     });
 
-    // firebase.database().ref('users/' + '123').remove()
+    // remove -> 刪除指定資料
+    firebase.database().ref('users/' + '123').remove()
 
 });
 
